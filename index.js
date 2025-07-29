31e0c30db1cb85d6b3536c3fe31959007c41908b
AndresFSerranoPC <andresfserrano1@gmail.com>
2025-07-29 17:11:26 -0500

    adding cors

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import "dotenv/config";
 import express from "express";
 import databaseConnection from "./src/db/config.js";
 import userRouter from "./src/routes/user.route.js"
+import cors from "cors";
 const app = express();
 databaseConnection();
 const PORT = process.env.PORT || 3001;
+app.use(cors());
 app.use(express.json());
 app.use("/users" , userRouter);
+
 app.listen(PORT,()=> {
     console.log("Servidor en funcionamiento en el puerto",PORT);
 });
